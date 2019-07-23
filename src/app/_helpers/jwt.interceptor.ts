500406ce966e38bd5fd0a2b663f45a359e5c8380
Luan Henrique <luan373@gmail.com>
2019-07-22 23:03:39 -0300

    Corrigido bug que adicionava duas vezes do Bearer

--- src/app/_helpers/jwt.interceptor.ts
+++ src/app/_helpers/jwt.interceptor.ts
@@ -13,7 +13,7 @@ export class JwtInterceptor implements HttpInterceptor {
         if (currentUser && currentUser.token) {
             request = request.clone({
                 setHeaders: {
-                    Authorization: `Bearer ${currentUser.token}`
+                    Authorization: `${currentUser.token}`
                 }
             });
         }
