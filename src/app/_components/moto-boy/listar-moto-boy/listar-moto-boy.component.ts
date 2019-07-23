83d1ba15ea26d19dadd51ad0504802cbb66b6a8c
Luan Henrique <luan373@gmail.com>
2019-07-22 23:04:09 -0300

    Adicionado motoBoy component e service

--- src/app/_components/moto-boy/listar-moto-boy/listar-moto-boy.component.ts
+++ src/app/_components/moto-boy/listar-moto-boy/listar-moto-boy.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ListarMotoBoyService } from 'src/app/_services/listar-moto-boy.service';
+import { MotoBoy } from 'src/app/_models/motoBoy';
+import { Subject, Observable, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-listar-moto-boy',
   templateUrl: './listar-moto-boy.component.html',
   styleUrls: ['./listar-moto-boy.component.css']
 })
-export class ListarMotoBoyComponent implements OnInit {
+export class ListarMotoBoyComponent implements OnInit, OnDestroy {
+  dtOptions: DataTables.Settings = {};
 
-  constructor() { }
+  dtTrigger: Subject<MotoBoy> = new Subject();
 
-  ngOnInit() {
+  listaMotoBoy: MotoBoy[] = [];
+
+  constructor(private listarMotoBoyService: ListarMotoBoyService) {
+
+  }
+
+  ngOnInit(): void {
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      processing: true    
+    };
+
+    this.listarMotoBoyService.lista().subscribe((data: []) => {
+      this.listaMotoBoy = data;
+
+      this.dtTrigger.next();
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    this.dtTrigger.unsubscribe();
   }
 
 }
